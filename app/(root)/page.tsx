01c7a683c3622ac7bf0dd3e62a86198843936f73
worthbeer <67826062+worthbeer@users.noreply.github.com>
2026-03-19 13:34:51 -0400

    impliment tailwind

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: "Bobo" }
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Here is your dashboard overview"
+          />
+          <TotalBalanceBox />
+        </header>
+      </div>
+    </section>
   )
 }
 
